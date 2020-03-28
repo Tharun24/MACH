b7001f85aab8eeeb1a1ffedb78192c9c1847ebc4
Tharun24 <tharunkumarreddy24@gmail.com>
2020-03-28 16:53:08 -0500

    removed external util folder and .gitignore

--- util/Gather.h
+++
@@ -1,3 +0,0 @@
-void cgather_batch(float*, long*, float*, int, int, int, int, int);
-void cgather_K(float*, long*, float*, int, int, int, int, int);
-void retrieve(float* raw, long* indices, float* result, int R, int B, int N, int M, int tdx, int THREADS);
